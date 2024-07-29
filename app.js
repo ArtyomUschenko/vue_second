d34ab5cc3fa9c06459bcd44ba7aeead79a17a723
Artyom Uschenko <net775@yandex.ru>
2024-07-29 16:55:39 +0300

    02 Основы Vue. Создаем двустороннее связывание (event)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const App = {
         return {
             placeholderString: "Введите название заметки",
             title: "Список заметок",
-            inputValue: ""
+            inputValue: "2222"
         }
     },
     methods: {
@@ -11,9 +11,34 @@ const App = {
             console.log(event.target.value);
             this.inputValue = event.target.value;
         }
+
     }
 }
 
 const app = Vue.createApp(App)
 
 app.mount("#app") //
+
+
+// Vue.createApp({
+//     data:function () {
+//         return {
+//             placeholderString: "Введите ваше имя",
+//             title: "Список заметок",
+//             inputValue: "",
+//             notes: []
+//         }
+//     },
+//     methods: {
+//         inputChangeHandler(event){
+//             this.inputValue = event.target.value;
+//         },
+//         addNewNote(){
+//             if(inputValue !="") {
+//                 this.notes.push(this.inputValue)
+//                 this.inputValue = ""
+//             }
+//         }
+//     }
+// })
+//     .month("#app")
