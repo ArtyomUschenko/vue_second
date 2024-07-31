151ca5ca1f9691f5ad13d7e20064f2d9a842ed09
Artyom Uschenko <net775@yandex.ru>
2024-07-31 12:14:42 +0300

    03 Что еще есть во Vue. 6 Зачем нужен key (v1)  stopPropagation - Отмена удаления

--- 03_lesson/app.js
+++ 03_lesson/app.js
@@ -21,5 +21,10 @@ Vue.createApp({
             age: 30
         },
         items: [1, 2, 3, 4, 5],
-    })
+    }),
+    methods: {
+        stopPropagation(event) {
+            event.stopPropagation()
+        }
+    }
 }).mount("#app")
