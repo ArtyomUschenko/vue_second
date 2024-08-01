60b021958f51385a7f5583313de94b8908a868c9
Artyom Uschenko <net775@yandex.ru>
2024-08-01 14:13:20 +0300

    04 Как работает Vue. 2 Virtual DOM и render

--- 04_lesson/app.js
+++ 04_lesson/app.js
@@ -1,14 +1,30 @@
+const  h = Vue.h
+
 const app = Vue.createApp({
    data() {
        return {
            title: "ГОСЭДО"
        }
    },
-    template: `<div class="card center">
-                    <h1>{{ title }}</h1>
-                    <button class="btn" @click="title='Информация'">Записать</button>
-                </div>
-`
+    methods: {
+       changeTitle() {
+           this.title = "Информация"
+       }
+    },
+//     template: `<div class="card center">
+//                     <h1>{{ title }}</h1>
+//                     <button class="btn" @click="title='Информация'">Записать</button>
+//                 </div>
+// `
+    render(){
+       return h("div", {
+           class: "card center"
+       }, [
+           h("h1", {}, this.title),
+           h("button", {class: "btn",
+           onclick: this.changeTitle}, "Записать" ),
+       ])
+    }
 })
 
 app.mount('#app')
